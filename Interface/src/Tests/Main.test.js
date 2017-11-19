174fc715ce54542ff9f4bc9e03b1a59f4a4724c3
kadhirvelm <kadhirvelm@berkeley.edu>
2017-11-19 15:25:54 -0800

    Updated
    Dockerized all images
    
    Jest added
    
    Jest added
    
    Node modules removed
    
    Tests removed and simplified - CI integration ready
    
    Travis file added
    
    Travis building and deploying added
    
    Added to master branch
    
    Adjusted travis
    
    Building conditional
    
    Fixed

---
+++ Interface/src/Tests/Main.test.js
@@ -0,0 +1,5 @@
+/* global it, expect */
+
+it('Returns true', () => {
+  expect(1).toEqual(1)
+})
--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -1,5 +1,5 @@
 const roomRoutes = require('./room_routes');
 
 module.exports = function(app, io, db){
-	roomRoutes(app, io, db);	
+	roomRoutes(app, io, db);
 }
