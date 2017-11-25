522b94b44fa12d7e343e54a13bcbcb6736b2d636
kadhirvelm <kadhirvelm@berkeley.edu>
2017-11-25 00:47:37 -0800

    Chmod .pem

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 eval "$(ssh-agent -s)";
 ssh-add "Political-Capital.pem"
+chmod 400 "Political-Capital.pem"
 ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazonaws.com << EOF
     cd ./political-capital;
     git pull;
