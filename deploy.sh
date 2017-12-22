913fa2059b5a383ddf4b4ee95492653ee6811db6
kadhirvelm <kadhirvelm@berkeley.edu>
2017-12-21 23:11:56 -0800

    Running the app in the background

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazon
     sudo docker stop $(sudo docker ps -aq);
     sudo docker system prune -a -f;
     sudo docker build . -t political-capital-server:latest;
-    sudo docker run -p 3000:3000 political-capital-server;
+    sudo docker run -d -p 3000:3000 political-capital-server;
     exit;
 EOF
