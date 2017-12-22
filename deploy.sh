d1b31e9624516395fc287332679774b18d45045e
kadhirvelm <kadhirvelm@berkeley.edu>
2017-12-21 22:11:13 -0800

    docker compose added

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,9 @@ chmod 400 "Political-Capital.pem"
 ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazonaws.com << EOF
     cd ./political-capital;
     git pull;
-    cd ./Server;
     sudo docker rm $(sudo docker ps -a -q);
     sudo docker rmi $(sudo docker images -q);
-    sudo docker build . -t political-capital:latest;
-    sudo docker run political-capital:latest;
-    pm2 reload all;
+    sudo docker-compose build server;
+    sudo docker-compose up server;
     exit;
 EOF
