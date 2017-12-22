a532b663a7592625b4a93a22caaa8cd2c7b7a7e5
kadhirvelm <kadhirvelm@berkeley.edu>
2017-12-21 22:22:13 -0800

    no daemon added

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ chmod 400 "Political-Capital.pem"
 ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazonaws.com << EOF
     cd ./political-capital;
     git pull;
+    cd ./Server;
     sudo docker rm $(sudo docker ps -a -q);
     sudo docker rmi $(sudo docker images -q);
-    sudo docker-compose build server;
-    sudo docker-compose up server;
+    sudo docker build . -t political-capital-server:latest;
+    sudo docker run political-capital-server:latest;
     exit;
 EOF
