23cd62485a823a1dc0d3dc4a10c7291122daef53
kadhirvelm <kadhirvelm@berkeley.edu>
2017-12-21 21:34:56 -0800

    pm2 docker added

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazon
     cd ./political-capital;
     git pull;
     cd ./Server;
+    sudo docker rm $(sudo docker ps -a -q)
+    sudo docker rmi $(sudo docker images -q)
     sudo docker build .;
     sudo docker run $(sudo docker ps -aq);
     pm2 reload all;
