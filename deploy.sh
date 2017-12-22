e1c97227269b3079cc6ea2db5c2398f4fd1218cf
kadhirvelm <kadhirvelm@berkeley.edu>
2017-12-21 21:47:15 -0800

    renamed to political capital latest

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazon
     cd ./Server;
     sudo docker rm $(sudo docker ps -a -q);
     sudo docker rmi $(sudo docker images -q);
-    sudo docker build .;
-    sudo docker run $(sudo docker ps -aq);
+    sudo docker build . -t political-capital:latest;
+    sudo docker run political-capital:latest;
     pm2 reload all;
     exit;
 EOF
