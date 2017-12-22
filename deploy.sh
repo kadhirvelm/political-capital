019980d9913a341f6bd9a0a4b85680520e372941
kadhirvelm <kadhirvelm@berkeley.edu>
2017-12-21 22:57:45 -0800

    deploy changed

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazon
     cd ./political-capital;
     git pull;
     cd ./Server;
-    sudo docker rm -f $(sudo docker ps -a -q);
-    sudo docker rmi -f $(sudo docker images -q);
+    sudo docker stop $(sudo docker ps -aq);
+    sudo docker system prune -a -f;
     sudo docker build . -t political-capital-server:latest;
     sudo docker run -p 3000:3000 political-capital-server;
     exit;
