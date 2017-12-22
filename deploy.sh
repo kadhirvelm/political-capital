ea8a5ba86943d506367d9111356e92e2a86849da
kadhirvelm <kadhirvelm@berkeley.edu>
2017-12-21 23:32:38 -0800

    version incremented to 0.3.0

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazon
     cd ./political-capital;
     git pull;
     cd ./Server;
-    sudo docker stop $(sudo docker ps -aq);
-    sudo docker system prune -a -f;
+    sudo docker stop political-capital-server;
     sudo docker build . -t political-capital-server:latest;
-    sudo docker run -d -p 3000:3000 political-capital-server;
+    sudo docker run --name=political-capital-server --restart=always -d -p 3000:3000 political-capital-server;
+    sudo docker system prune -a -f;
     exit;
 EOF
