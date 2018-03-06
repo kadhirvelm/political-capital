5e75d74bf9d2f6ccbec2daed64b2a6d08975a02b
kadhirvelm <kadhirvelm@berkeley.edu>
2018-03-05 22:37:12 -0800

    Composition changes, some travis deploy changes

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazon
     git pull;
     cd ./Server;
     sudo docker stop political-capital-server;
+    sudo docker system prune -a -f;
     sudo docker build . -t political-capital-server:latest;
     sudo docker run --name=political-capital-server --restart=always -d -p 3000:3000 political-capital-server;
-    sudo docker system prune -a -f;
     exit;
 EOF
