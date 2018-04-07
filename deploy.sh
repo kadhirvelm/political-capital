12b157b63193a5191e4bac7b15b0ee51d787642e
kadhirvelm <kadhirvelm@berkeley.edu>
2018-04-07 13:06:14 -0600

    Deploy server added

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 eval "$(ssh-agent -s)";
 chmod 400 "Political-Capital.pem"
 ssh-add "Political-Capital.pem"
-ssh -i "Political-Capital.pem" ubuntu@ec2-54-193-47-210.us-west-1.compute.amazonaws.com << EOF
+ssh -i "Political-Capital.pem" ubuntu@ec2-13-57-247-210.us-west-1.compute.amazonaws.com << EOF
     cd ./political-capital;
     git pull;
     cd ./Server;
